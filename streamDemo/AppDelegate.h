0c7e53c3ce0479817359c7fea0d04a2e1208a3c3
Andrii Tishchenko <andrii.tishchenko+github@gmail.com>
2015-12-25 18:50:06 +0200

    Initial Commit

---
+++ streamDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  streamDemo
+//
+//  Created by Andrii Tishchenko on 25.12.15.
+//  Copyright © 2015 Andrii Tishchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ streamDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  streamDemo
+//
+//  Created by Andrii Tishchenko on 25.12.15.
+//  Copyright © 2015 Andrii Tishchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
