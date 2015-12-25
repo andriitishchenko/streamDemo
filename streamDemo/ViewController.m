37e3c6058a4b68680899e332738cf2c74f854086
Andrii Tishchenko <andrii.tishchenko+github@gmail.com>
2015-12-25 19:23:22 +0200

    inital

--- streamDemo/ViewController.m
+++ streamDemo/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIWebViewDelegate,UITextFieldDelegate>
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) IBOutlet UITextField *textfieldURL;
 
 @end
 
@@ -17,11 +19,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    self.webView ope
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)button_gourl_click:(id)sender {
+    [self go];
+}
+
+-(void)go
+{
+    [self.textfieldURL resignFirstResponder];
+    NSURL*url = [[NSURL alloc] initWithString:self.textfieldURL.text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url
+                                             cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:30];
+    [self.webView loadRequest:request];
+
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField*)textField;
+{
+    [textField resignFirstResponder];
+    [self go];
+    return YES;
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    [webView loadHTMLString:[error debugDescription] baseURL:nil];
 }
 
 @end
